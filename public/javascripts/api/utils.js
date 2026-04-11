b05e98890d23252cb90a0766b1eff31d95d4b463
Legend <thesfmgod@gmail.com>
2026-04-11 17:07:24 -0400

    test(api): TMDb support in progress
    BREAKING CHANGE expect errors and website crashes... API implementation is in very early development.

---
+++ public/javascripts/api/utils.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import config from '../../../config.json';
+
+/**
+ * Initialize API from TMDB
+ * For more info: https://developers.themoviedb.org/
+ */
+export const API_KEY = config.api_key;
+
+/**
+ * Get API URLS from TMDB.
+ * For more info: https://developers.themoviedb.org/ (watchlists)
+ * For more info: https://developers.themoviedb.org/ (now playing)
+ */
+export const API_URL = await Promise.all([
+     axios.get(`https://api.themoviedb.org/3/account/22066702/watchlist`),
+     axios.get(`https://api.themoviedb.org/3/movie/now_playing`),
+]);
