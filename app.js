a47101acc91b3da2a1a24f929394c550561b5a6f
Legend <thesfmgod@gmail.com>
2026-04-12 23:27:20 -0400

    fix(application): create temp fallback for error 404 handling

--- app.js
+++ app.js
@@ -59,13 +59,8 @@ app.use(function (req, res, next) { next(createError(404)); });
  * @param {*} next - Next middleware
  */
 app.use(function (err, req, res, next) {
-     // set locals, only providing error in development.
-     res.locals.message = err.message;
-     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-     // render the error page.
-     res.status(err.status || 500);
-     res.render('error');
+     // [SUBJECT TO CHANGE] - Change status code and error message.
+     res.status(err.status || 500).send(err.message || 'Internal Server Error');
 });
 
 export default app;
